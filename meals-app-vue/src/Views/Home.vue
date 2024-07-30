fff549d05fac88211a1ca986335f3d872b2982a1
Ali Zaidi <156838198+AliZaidi2403@users.noreply.github.com>
2024-07-30 07:19:35 +0530

    Update Home.vue

--- meals-app-vue/src/Views/Home.vue
+++ meals-app-vue/src/Views/Home.vue
@@ -18,4 +18,5 @@ onMounted(async () => {
     api.get(`random.php`).then(({ data }) => meals.value.push(data.meals[0]));
   }
 });
+//A computed property is used to declaratively describe a value that depends on other values. When you data-bind to a computed property inside the template, Vue knows when to update the DOM when any of the values depended upon by the computed property has changed
 </script>
